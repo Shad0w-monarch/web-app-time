a3cefd2fedc775639f128eaa9ae1e58b081437b9
Shad0w-monarch <98059194+Shad0w-monarch@users.noreply.github.com>
2022-01-24 20:30:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WELCOME PAGE</title>
+  <!link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Welcome</h2>
+  </div>
+
+	  <!Transfer method aswell as target file for data(****) submission>
+  <form method="post" action="****">
+
+       <!labels for data>
+  	<div class="input-group">
+  		<label>Enter your name</label>
+  		<input type="text" name="username" >
+  	</div>
+
+	  <!labels for submit action>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="submit">Submit</button>
+  	</div>
+  </form>
+</body>
+</html>
