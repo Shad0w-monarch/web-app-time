7001c738e184002b6685c22cc51ad8430caaf984
ZedJahad428 <68990535+ZedJahad428@users.noreply.github.com>
2022-02-15 12:30:48 +0200

    Create challenge.php
    This page is meant to bounce users from accessing the service before a stated time. This is just the basic shell of the page, refinement will follow

---
+++ challenge.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page closed !!</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>The page you are trying to access is currently closed
+        <br>
+        <br>
+      please come back in: </P></h2>
+      <br>
+      <br>
+      <!--insert timer down here-->
+  </div>
+
+</body>
+</html>
+<!--<footer>
+    <script>
+        src='timer.js'
+        #assumingYouGoWithJavaScript
+    </script>
+</footer>-->
